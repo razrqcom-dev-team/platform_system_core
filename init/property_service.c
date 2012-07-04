642540624e6cfcf889a3fad3afeeb87a9954ccaf
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-07-04 15:26:57 +0100

    init: Add a mechanism to extend the property service in device configs
    Instead of every so often adding a new set of stuff here for a specific
    device, let the device includes overload the property arrays. The
    new device_perms.h header documents it
    
    Change-Id: I477e27abebeb4b85abe5fcaf19333999b1625417
    
    Conflicts:
    	init/property_service.c

--- init/property_service.c
+++ init/property_service.c
@@ -48,6 +48,8 @@
 #include "util.h"
 #include "log.h"
 
+#include <device_perms.h>
+
 #define PERSISTENT_PROPERTY_DIR  "/data/property"
 
 static int persistent_properties_loaded = 0;
@@ -56,6 +58,7 @@ static int property_area_inited = 0;
 static int property_set_fd = -1;
 
 /* White list of permissions for setting property services. */
+#ifndef PROPERTY_PERMS
 struct {
     const char *prefix;
     unsigned int uid;
@@ -94,11 +97,14 @@ struct {
     { "selinux."         , AID_SYSTEM,   0 },
     { NULL, 0, 0 }
 };
+/* Avoid extending this array. Check device_perms.h */
+#endif
 
 /*
  * White list of UID that are allowed to start/stop services.
  * Currently there are no user apps that require.
  */
+#ifndef CONTROL_PERMS
 struct {
     const char *service;
     unsigned int uid;
@@ -108,6 +114,8 @@ struct {
     { "ril-daemon",AID_RADIO, AID_RADIO },
      {NULL, 0, 0 }
 };
+/* Avoid extending this array. Check device_perms.h */
+#endif
 
 typedef struct {
     void *data;
